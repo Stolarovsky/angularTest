c770b2cc706f39078c58ec06f274fa3a8fc8670a
Stolarovsky <stolarczyk.ozimek@gmail.com>
2023-06-14 09:04:59 +0200

    life cycle hooks

--- testApp/src/app/new-component-test/new-component-test.component.ts
+++ testApp/src/app/new-component-test/new-component-test.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-new-component-test',
   templateUrl: './new-component-test.component.html',
   styleUrls: ['./new-component-test.component.css']
 })
-export class NewComponentTestComponent {
+export class NewComponentTestComponent implements OnInit, OnDestroy {
   name = 'Anonymous';
   printing = false;
-  persons = [{ name: 'Anonymous', age: '99' }, { name: 'Person_1', age: '28' }, { name: 'Person_2', age: '35' }];
+  persons = [{ name: "", age: "" }];
   customSubmit = (event: Event) => {
     console.log(`Poszło!`);
     console.log(event)
     this.printing = !this.printing
+  };
+  ngOnInit() {
+    console.log('Component init!')
+    this.persons = [{ name: 'Anonymous', age: '99' }, { name: 'Person_1', age: '28' }, { name: 'Person_2', age: '35' }];
+  };
+  ngOnDestroy() {
+    console.log('Component destroyed!')
   };
 }
